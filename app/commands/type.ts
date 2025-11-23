52c054ee3272b0974eedc00b1ee5ef2db6899ecb
podaleks <podaleks78@gmail.com>
2025-11-23 15:06:45 +0300

    find executable files

--- app/commands/type.ts
+++ app/commands/type.ts
@@ -1,8 +1,10 @@
 import type { Interface } from "readline";
 import { getFirstWordAndRest } from "../lib";
 import { COMMANDS } from "./enums";
+import { accessSync, constants } from 'fs'
+import path from "path";
 
-export function type(rl: Interface, args: string) {
+export async function type(rl: Interface, args: string) {
     const [command] = getFirstWordAndRest(args)
 
     switch (command) {
@@ -12,6 +14,26 @@ export function type(rl: Interface, args: string) {
             rl.write(`${command} is a shell builtin\n`)
             break;
         default:
-            rl.write(`${args}: not found\n`)
+            const paths = process.env.PATH?.split(path.delimiter)!
+
+            const fullPath = paths.find((item) => {
+                const fullPath = path.join(item, command)
+
+                try {
+                    accessSync(fullPath, constants.F_OK);
+                    accessSync(fullPath, constants.X_OK);
+
+                    return true
+                } catch (e) {
+                    return false
+                }
+            })
+
+            if (fullPath) {
+                rl.write(`${command} is ${path.join(fullPath, command)}\n`)
+            } else {
+                rl.write(`${command}: not found\n`)
+            }
+    
     }
 }
