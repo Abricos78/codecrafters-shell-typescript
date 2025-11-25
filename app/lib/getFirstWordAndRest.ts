c94da46ec4f426a5903787d0bb6c5e7137f09926
podaleks <podaleks78@gmail.com>
2025-11-26 00:24:24 +0300

    implement command string

--- app/lib/getFirstWordAndRest.ts
+++ app/lib/getFirstWordAndRest.ts
@@ -1,3 +1,21 @@
 export function getFirstWordAndRest(str: string) {
-    return str.split(/(?<=^\S+)\s/)
+    const result = []
+    if (str.startsWith('\'') || str.startsWith('"')) {
+        let command = ''
+        let quoeStack = [str[0]]
+
+        for (let i = 1; i < str.length; i++) {
+            if (str[i] === quoeStack[0]) {
+                result.push(command)
+                result.push(...str.substring(i + 1).split(/(?<=^\S+)\s/)!)
+                return result
+            }
+            command += str[i]
+        }
+
+        return [command, '']
+
+    } else {
+        return str.split(/(?<=^\S+)\s/)
+    }
 }
